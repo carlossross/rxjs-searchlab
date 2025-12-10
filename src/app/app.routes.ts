b68817c19309c8656cf35929c18c641623c98419
carlossross <carlos.rossv@gmail.com>
2025-12-10 12:43:45 -0600

    chore: Project init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { SearchPageComponent } from './search/search-page/search-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: SearchPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
